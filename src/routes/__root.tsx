35696c8b9432b7dc8b06027d95414c2684b6185f
Camden Mecklem <c@camdenmecklem.com>
2026-03-18 16:08:55 -0400

    remove tanstack router dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const rootRoute = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
     </>
   ),
 })
