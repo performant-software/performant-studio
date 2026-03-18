e86ce0f293b62fe07a0bde37b0582a2422203bce
Camden Mecklem <c@camdenmecklem.com>
2026-03-18 14:05:37 -0400

    remove sso route

--- src/routeTree.ts
+++ src/routeTree.ts
@@ -1,5 +1,4 @@
 import { rootRoute } from './routes/__root'
 import { indexRoute } from './routes/index'
-import { ssoRoute } from './routes/sso'
 
-export const routeTree = rootRoute.addChildren([indexRoute, ssoRoute])
+export const routeTree = rootRoute.addChildren([indexRoute])
--- src/routes/sso.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from '@tanstack/react-router'
-import Sso from '../components/Sso'
-import { rootRoute } from './__root.tsx'
-
-export const ssoRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: '/sso',
-  component: Sso,
-})
